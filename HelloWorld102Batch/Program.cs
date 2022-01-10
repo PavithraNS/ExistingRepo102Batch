4756b5ee737a9fa6975413f4ddf7e6e0ced2b5ab
PavithraNS <nspavi1991@gmail.com>
2022-01-10 13:05:25 +0530

    [Pavithra] Adding HelloWorld project

---
+++ HelloWorld102Batch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld102Batch
+{
+    /// <summary>
+    /// This is an entry class for console Application
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Console.ReadLine();
+        }
+    }
+}
